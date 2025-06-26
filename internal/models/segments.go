f8ab52a8ef96d724fed49660b48703e3e2c41deb
Alif Rahmadian <alifrah78@yahoo.com>
2025-06-27 01:19:05 +0700

    feat: add segment model

--- internal/models/segments.go
+++ internal/models/segments.go
@@ -1,8 +1,37 @@
 package models
 
+import "time"
+
 type Segment struct {
-	Type string `json:"@type"`
+	ID                          int64                   `json:"id"`
+	Type                        string                  `json:"@type"`
+	SegmentOfferInformation     SegmentOfferInformation `json:"segmentOfferInformation"`
+	Duration                    uint                    `json:"duration"`
+	CabinClass                  string                  `json:"cabinClass"`
+	Equipment                   string                  `json:"equipment"`
+	Flight                      Flight                  `json:"flight"`
+	Origin                      string                  `json:"origin"`
+	Destination                 string                  `json:"destination"`
+	Departure                   time.Time               `json:"departure"`
+	Arrival                     time.Time               `json:"arrival"`
+	BookingClass                string                  `json:"bookingClass"`
+	LayoverDuration             uint                    `json:"layoverDuration"`
+	FareBasis                   string                  `json:"fareBasis"`
+	SubjectToGovernmentApproval bool                    `json:"subjectToGovernmentApproval"`
+	SegmentRef                  string                  `json:"segmentRef"`
+}
 
-	// Segment Offer Information Field
+type SegmentOfferInformation struct {
+	FlightMiles uint64 `json:"flightMiles"`
+	AwardFlight bool   `json:"awardFlight"`
+}
 
+type Flight struct {
+	FlightNumber          uint     `json:"flightNumber"`
+	OperatingFlightNumber uint     `json:"operatingFlightNumber"`
+	AirlineCode           string   `json:"airlineCode"`
+	OperatingAirlineCode  string   `json:"operatingAirlineCode"`
+	StopAirports          []string `json:"stopAirports"`
+	DepartureTerminal     string   `json:"departureTerminal"`
+	ArrivalTerminal       string   `json:"arrivalTerminal"`
 }
