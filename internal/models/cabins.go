19694d46cb27dca3734df2eb23595ce7cea21658
Alif Rahmadian <alifrah78@yahoo.com>
2025-06-28 11:17:00 +0700

    add SeatMap on Cabin model

--- internal/models/cabins.go
+++ internal/models/cabins.go
@@ -3,6 +3,7 @@ package models
 type Cabin struct {
 	ID          int64    `json:"id"`
 	SeatMapID   int64    `json:"seat_map_id"`
+	SeatMap     SeatMap  `json:"seat_map"`
 	Deck        string   `json:"deck"`
 	SeatColumns []string `json:"seat_columns"`
 	FirstRow    uint     `json:"first_row"`
