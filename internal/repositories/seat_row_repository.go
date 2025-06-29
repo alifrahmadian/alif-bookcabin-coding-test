2cfda66939fff8442b5f71f4734942d462b922a0
Alif Rahmadian <alifrah78@yahoo.com>
2025-06-29 13:40:24 +0700

    feat: add get seat row by cabin id repository

---
+++ internal/repositories/seat_row_repository.go
@@ -0,0 +1,59 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/alifrahmadian/alif-bookcabin-coding-test/internal/models"
+)
+
+type SeatRowRepository interface {
+	GetSeatRowsByCabinID(id int64) ([]*models.SeatRow, error)
+}
+
+type seatRowRepository struct {
+	DB *sql.DB
+}
+
+func NewSeatRowRepository(db *sql.DB) SeatRowRepository {
+	return &seatRowRepository{
+		DB: db,
+	}
+}
+
+func (r *seatRowRepository) GetSeatRowsByCabinID(id int64) ([]*models.SeatRow, error) {
+	var seatRows []*models.SeatRow
+
+	query := `
+		SELECT
+			id,
+			cabin_id,
+			row_number,
+			seat_codes
+		FROM
+			seat_row
+		WHERE cabin_id = $1
+	`
+
+	rows, err := r.DB.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		seatRow := &models.SeatRow{}
+		err := rows.Scan(
+			&seatRow.ID,
+			&seatRow.Cabin.SeatColumns,
+			&seatRow.RowNumber,
+			&seatRow.SeatCodes,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		seatRows = append(seatRows, seatRow)
+	}
+
+	return seatRows, nil
+}
