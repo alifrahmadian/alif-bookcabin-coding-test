1e427c76284ec6fc8b77c164f8cce6a879becef9
Alif Rahmadian <alifrah78@yahoo.com>
2025-06-29 12:57:29 +0700

    feat: add get seat map by id repository

---
+++ internal/repositories/seat_map_repository.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/alifrahmadian/alif-bookcabin-coding-test/internal/models"
+)
+
+type SeatMapRepository interface {
+	GetSeatMapByID(id int64) (*models.SeatMap, error)
+}
+
+type seatMapRepository struct {
+	DB *sql.DB
+}
+
+func NewSeatMapRepository(db *sql.DB) SeatMapRepository {
+	return &seatMapRepository{
+		DB: db,
+	}
+}
+
+func (r *seatMapRepository) GetSeatMapByID(id int64) (*models.SeatMap, error) {
+	seatMap := &models.SeatMap{}
+
+	query := `
+		SELECT
+			id,
+			segment_id,
+			rows_disabled_causes,
+			aircraft
+		FROM
+			seat_map
+		WHERE
+			id = $1
+	`
+
+	err := r.DB.
+		QueryRow(query, id).
+		Scan(
+			&seatMap.ID,
+			&seatMap.SegmentID,
+			&seatMap.RowsDisabledCauses,
+			&seatMap.Aircraft,
+		)
+	if err != nil {
+		return nil, err
+	}
+
+	return seatMap, nil
+}
